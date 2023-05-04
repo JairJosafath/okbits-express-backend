e8e6c4e355a237e88e8f2ec96d49babbf87fd2cb
HungryCompiler <58106977+JairJosafath@users.noreply.github.com>
2023-05-04 04:45:36 -0300

    didnt export the new sequelize instance

--- app/config/db.ts
+++ app/config/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || "");
+export const sequelize = new Sequelize(process.env.DATABASE_URL || "");
 
 export async function testdb() {
   try {
